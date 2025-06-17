b5ba731a9f9243b3e533041c905739a4a7cd6070
leejunghun1234 <126745597+leejunghun1234@users.noreply.github.com>
2025-06-17 16:02:40 +0900

    Delete src directory

--- src/click.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three';
-
-export function raycaster() {
-    const raycaster = new THREE.Raycaster();
-    const mouse = new THREE.Vector2();
-    let selectedObject = undefined;
-    
-}
